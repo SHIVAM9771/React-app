ff5715db75fd9b66a2469379e4b150a9c3c00a38
Shivam kumar <code.python.51@gmail.com>
2024-10-05 16:23:52 +0530

    pushed all files

---
+++ my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import "bootstrap/dist/css/bootstrap.min.css"
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
+}
+
