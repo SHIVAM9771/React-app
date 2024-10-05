1d2cc3763324dfa511caf1d3d3cd5a46012da389
Shivam kumar <code.python.51@gmail.com>
2024-10-05 18:02:28 +0530

    added function using useState

--- my-app/src/components/TextForm.jsx
+++ my-app/src/components/TextForm.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 export default function (props) {
+    const handleUpClick = () => {
+        let newText = text.toUpperCase();
+        setText(newText);
+    }
+
+    const handleOnChange = (event) => {
+        setText(event.target.value);
+    }
+
+    const [text, setText] = useState("Enter your Text here..");
     return (
         <div>
             <h1 className='my-3 mt-4 text-center'>{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" id="myBox" rows="8"></textarea>
+                <textarea className="form-control" id="myBox" rows="8" value={text} onChange={handleOnChange} ></textarea>
             </div>
-            <button className="btn btn-primary">Convert to Uppercase</button>
+            <button className="btn btn-primary" onClick={handleUpClick} >Convert to Uppercase</button>
         </div>
     );
 }
