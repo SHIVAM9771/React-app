5e8b6258bc5b25129cc7525e72acf0e7df1a827d
Shivam kumar <code.python.51@gmail.com>
2024-10-05 21:43:06 +0530

    Indentation updated to 2

--- my-app/src/components/TextForm.jsx
+++ my-app/src/components/TextForm.jsx
@@ -6,19 +6,37 @@ export default function (props) {
         let newText = text.toUpperCase();
         setText(newText);
     }
+    const handleDownClick = () => {
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
 
     const handleOnChange = (event) => {
         setText(event.target.value);
     }
 
     const [text, setText] = useState("Enter your Text here..");
+
+
     return (
-        <div>
-            <h1 className='my-3 mt-4 text-center'>{props.heading}</h1>
-            <div className="mb-3">
-                <textarea className="form-control" id="myBox" rows="8" value={text} onChange={handleOnChange} ></textarea>
+        <>
+            <div>
+                <h1 className='my-3 mt-4 text-center'>{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea className="form-control" id="myBox" rows="8" value={text} onChange={handleOnChange} ></textarea>
+                </div>
+                <button className="btn btn-primary mx-1" onClick={handleUpClick} >Convert to Uppercase</button>
+                <button className="btn btn-primary mx-2" onClick={handleDownClick} >Convert to Lowercase</button>
             </div>
-            <button className="btn btn-primary" onClick={handleUpClick} >Convert to Uppercase</button>
-        </div>
+
+            <div className="container my-4">
+                <h1>Your Text Summary</h1>
+                <p><strong>{text.split(" ").length}</strong> words and <strong>{text.length}</strong> characters</p>
+                <p><strong>{0.008 * text.split(" ").length}</strong> Minutes read</p>
+                <h2>Preview</h2>
+                <p>{text}</p>
+            </div>
+
+        </>
     );
 }
